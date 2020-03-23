bf034f9af94fdc78063018b3b8d896ed33bb1ff5
fidaZuhour <fidanasser8@gmail.com>
2020-03-23 20:02:13 +0200

    simplify with chat

--- lib/screens/notification_screen.dart
+++ lib/screens/notification_screen.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/material.dart';
 
 final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
 
